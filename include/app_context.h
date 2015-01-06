ef4a0da5045e41998d4626593d9841e5bab1fadf
Sangyoon Jang <s89.jang@samsung.com>
2015-01-06 14:08:20 +0900

    Update source from tizen 2.3
    Change-Id: Iada103642380db28f880bae5ccc20f1b1288fcbe
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

--- include/app_context.h
+++ include/app_context.h
@@ -11,10 +11,10 @@
  * distributed under the License is distributed on an AS IS BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 
- 
+
 #ifndef __TIZEN_APPFW_APP_CONTEXT_H
 #define __TIZEN_APPFW_APP_CONTEXT_H
 
@@ -25,41 +25,42 @@
 extern "C" {
 #endif
 
-
 /**
- * @defgroup CAPI_APP_CONTEXT_MODULE Application Context
- * @ingroup CAPI_APPLICATION_MANAGER_MODULE
+ * @file  app_context.h
  */
 
-
 /**
- * @addtogroup CAPI_APP_CONTEXT_MODULE
+ * @addtogroup  CAPI_APP_CONTEXT_MODULE
  * @{
  */
 
 
 /**
- * @brief Application context handle.
+ * @brief  Application context handle.
+ * @since_tizen  2.3
  */
 typedef struct app_context_s *app_context_h;
 
 
 /**
- * @brief Enumerations of event type for the application context event
+ * @brief  Enumeration for Application Context Event.
+ * @since_tizen  2.3
  */
 typedef enum
 {
-	APP_CONTEXT_EVENT_LAUNCHED, /**< The application is launched */
-	APP_CONTEXT_EVENT_TERMINATED, /**< The application is terminated */
+    APP_CONTEXT_EVENT_LAUNCHED, /**< The application is launched */
+    APP_CONTEXT_EVENT_TERMINATED, /**< The application is terminated */
 } app_context_event_e;
 
 
 /**
- * @brief Destroys the application context handle and releases all its resources.
- * @param [in] app_context The application context handle
- * @return 0 on success, otherwise a negative error value.
- * @retval #APP_MANAGER_ERROR_NONE Successful
- * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
+ * @brief  Destroys the application context handle and releases all its resources.
+ * @since_tizen 2.3
+ * @param[in]   app_context  The application context handle
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
  * @see app_manager_foreach_app_context()
  * @see app_manager_get_app_context()
  */
@@ -67,83 +68,97 @@ int app_context_destroy(app_context_h app_context);
 
 
 /**
- * @brief Gets the package with the given application context.
- * @remark This function is @b deprecated. Use app_context_get_app_id() instead.
- * @remarks @a package must be released with free() by you.
- * @param [in] app_context The application context
- * @param [out] package The package of the given application context
- * @return 0 on success, otherwise a negative error value.
- * @retval #APP_MANAGER_ERROR_NONE Successful
- * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
- * @retval #APP_MANAGER_ERROR_DB_FAILED Database error occurred
- * @retval #APP_MANAGER_ERROR_OUT_OF_MEMORY Out of memory
+ * @brief    Gets the package with the given application context.
+ * @since_tizen 2.3
+ * @remarks  This function is @b deprecated. Use app_context_get_app_id() instead. \n
+ *           You must release @a package using free().
+ * @param[in]   app_context  The application context
+ * @param[out]  package      The package of the given application context
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
+ * @retval  #APP_MANAGER_ERROR_DB_FAILED          Database error occurred
+ * @retval  #APP_MANAGER_ERROR_OUT_OF_MEMORY      Out of memory
  */
 int app_context_get_package(app_context_h app_context, char **package);
 
 
 /**
- * @brief Gets the application ID with the given application context.
- * @remarks @a app_id must be released with free() by you.
- * @param [in] app_context The application context
- * @param [out] app_id The application ID of the given application context
- * @return 0 on success, otherwise a negative error value.
- * @retval #APP_MANAGER_ERROR_NONE Successful
- * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
- * @retval #APP_MANAGER_ERROR_DB_FAILED Database error occurred
- * @retval #APP_MANAGER_ERROR_OUT_OF_MEMORY Out of memory
+ * @brief    Gets the application ID with the given application context.
+ * @since_tizen 2.3
+ * @remarks     You must release @a app_id using free().
+ * @param[in]   app_context  The application context
+ * @param[out]  app_id       The application ID of the given application context
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
+ * @retval  #APP_MANAGER_ERROR_DB_FAILED          Database error occurred
+ * @retval  #APP_MANAGER_ERROR_OUT_OF_MEMORY      Out of memory
  */
 int app_context_get_app_id(app_context_h app_context, char **app_id);
 
 
 /**
- * @brief Gets the process ID with the given application context.
- * @param [in] app_context The application context
- * @param [out] pid The process ID of the given application context
- * @return 0 on success, otherwise a negative error value.
- * @retval #APP_MANAGER_ERROR_NONE Successful
- * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
- * @retval #APP_MANAGER_ERROR_DB_FAILED Database error occurred
- * @retval #APP_MANAGER_ERROR_OUT_OF_MEMORY Out of memory
+ * @brief  Gets the process ID with the given application context.
+ * @since_tizen 2.3
+ * @param[in]   app_context  The application context
+ * @param[out]  pid          The process ID of the given application context
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
+ * @retval  #APP_MANAGER_ERROR_DB_FAILED          Database error occurred
+ * @retval  #APP_MANAGER_ERROR_OUT_OF_MEMORY      Out of memory
  */
 int app_context_get_pid(app_context_h app_context, pid_t *pid);
 
 
 /**
- * @brief Checks whether the application with the given application context is terminated.
- * @param [in] app_context	The application context
- * @param [out] terminated true if the application is terminated, false if the application is running
- * @return 0 on success, otherwise a negative error value.
- * @retval #APP_MANAGER_ERROR_NONE Successful
- * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
- * @retval #APP_MANAGER_ERROR_DB_FAILED Database error occurred
- * @retval #APP_MANAGER_ERROR_OUT_OF_MEMORY Out of memory
+ * @brief  Checks whether the application with the given application context is terminated.
+ * @since_tizen 2.3
+ * @param[in]   app_context  The application context
+ * @param[out]  terminated   @c true if the application is terminated, \n
+ *                           otherwise @c false if the application is running
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
+ * @retval  #APP_MANAGER_ERROR_DB_FAILED          Database error occurred
+ * @retval  #APP_MANAGER_ERROR_OUT_OF_MEMORY      Out of memory
  */
 int app_context_is_terminated(app_context_h app_context, bool *terminated);
 
 
 /**
- * @brief Checks whether two application contexts are equal.
- * @param [in] lhs	The first application context to compare
- * @param [in] rhs	The second application context to compare
- * @param [out] equal true if the application contexts are equal, otherwise false
- * @return 0 on success, otherwise a negative error value.
- * @retval #APP_MANAGER_ERROR_NONE Successful
- * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
- * @retval #APP_MANAGER_ERROR_DB_FAILED Database error occurred
- * @retval #APP_MANAGER_ERROR_OUT_OF_MEMORY Out of memory
+ * @brief  Checks whether two application contexts are equal.
+ * @since_tizen 2.3
+ * @param[in]   lhs    The first application context to compare
+ * @param[in]   rhs    The second application context to compare
+ * @param[out]  equal  @c true if the application contexts are equal, \n
+ *                     otherwise @c false if they are not equal
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
+ * @retval  #APP_MANAGER_ERROR_DB_FAILED          Database error occurred
+ * @retval  #APP_MANAGER_ERROR_OUT_OF_MEMORY      Out of memory
  */
 int app_context_is_equal(app_context_h lhs, app_context_h rhs, bool *equal);
 
 
 /**
- * @brief Clones the application context handle.
- * @param [out] clone If successful, a newly created application context handle will be returned.
- * @param [in] app_context The application context
- * @return 0 on success, otherwise a negative error value.
- * @retval #APP_MANAGER_ERROR_NONE Successful
- * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
- * @retval #APP_MANAGER_ERROR_DB_FAILED Database error occurred
- * @retval #APP_MANAGER_ERROR_OUT_OF_MEMORY Out of memory
+ * @brief  Clones the application context handle.
+ * @since_tizen 2.3
+ * @param[out]  clone        The newly created application context handle, if cloning is successful
+ * @param[in]   app_context  The application context
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
+ * @retval  #APP_MANAGER_ERROR_DB_FAILED          Database error occurred
+ * @retval  #APP_MANAGER_ERROR_OUT_OF_MEMORY      Out of memory
  */
 int app_context_clone(app_context_h *clone, app_context_h app_context);
 
--- packaging/capi-appfw-app-manager.spec
+++ packaging/capi-appfw-app-manager.spec
@@ -1,35 +1,36 @@
 Name:       capi-appfw-app-manager
 Summary:    Application Manager API
-Version:    0.1.0
-Release:    0
+Version:    0.2.4
+Release:    1
 Group:      Application Framework/API
 License:    Apache-2.0
 Source0:    %{name}-%{version}.tar.gz
-Source1001:     capi-appfw-app-manager.manifest
 BuildRequires:  cmake
 BuildRequires:  pkgconfig(dlog)
 BuildRequires:  pkgconfig(vconf)
 BuildRequires:  pkgconfig(aul)
-BuildRequires:  pkgconfig(ail)
 BuildRequires:  pkgconfig(pkgmgr)
 BuildRequires:  pkgconfig(pkgmgr-info)
 BuildRequires:  pkgconfig(capi-base-common)
 BuildRequires:  pkgconfig(glib-2.0)
+Requires(post): /sbin/ldconfig
+Requires(postun): /sbin/ldconfig
 
 %description
 The Application Manager API provides functions to get information about running applications.
 
 %package devel
 Summary:  Application Manager API (Development)
-Group:    Application Framework/API
+Group:    Application Framework/Libraries
 Requires: %{name} = %{version}-%{release}
 
 %description devel
 The Application Manager API provides functions to get information about running applications. (DEV)
 
+
 %prep
 %setup -q
-cp %{SOURCE1001} .
+
 
 %build
 MAJORVER=`echo %{version} | awk 'BEGIN {FS="."}{print $1}'`
@@ -44,12 +45,15 @@ rm -rf %{buildroot}
 
 %postun -p /sbin/ldconfig
 
+
 %files
-%manifest %{name}.manifest
 %{_libdir}/libcapi-appfw-app-manager.so.*
+%{_bindir}/appmgr_tool
+%manifest %{name}.manifest
 
 %files devel
-%manifest %{name}.manifest
 %{_includedir}/appfw/*.h
 %{_libdir}/libcapi-appfw-app-manager.so
 %{_libdir}/pkgconfig/*.pc
+
+
