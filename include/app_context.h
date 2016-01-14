51edf04b0a8420166286b1ff7a121c3248cfffff
Myungki Lee <mk5004.lee@samsung.com>
2016-01-14 11:05:26 +0900

    Add new apis to get app_status
    - add api to get the full running app list
    - define app state enum
    - extend app_context struct
    - add api to get app_state, package id
    - modify about operation of resume_app
    
    Change-Id: Iec75d64f0abdb0ac6cdc1f9ed7eaa177c48a83ea
    Signed-off-by: Myungki Lee <mk5004.lee@samsung.com>

--- include/app_context.h
+++ include/app_context.h
@@ -54,6 +54,19 @@ typedef enum
 
 
 /**
+ * @brief Enumeration for the application state
+ * @since_tizen 3.0
+ */
+typedef enum {
+    APP_STATE_UNKNOWN, /**< The undefined state */
+    APP_STATE_FOREGROUND, /**< The UI application is running in the foreground. */
+    APP_STATE_BACKGROUND, /**< The UI application is running in the background. */
+    APP_STATE_SERVICE, /**< The Service application is running. */
+    APP_STATE_TERMINATED, /**< The application is terminated. */
+} app_state_e;
+
+
+/**
  * @brief  Destroys the application context handle and releases all its resources.
  * @since_tizen @if MOBILE 2.3 @elseif WEARABLE 2.3.1 @endif
  * @param[in]   app_context  The application context handle
@@ -112,6 +125,33 @@ int app_context_get_pid(app_context_h app_context, pid_t *pid);
 
 
 /**
+ * @brief  Gets the package ID with the given application context.
+ * @since_tizen 3.0
+ * @remarks     You must release @a pkg_id using free().
+ * @param[in]   app_context  The application context
+ * @param[out]  package_id   The package ID of the given application context
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
+ */
+int app_context_get_pkg_id(app_context_h app_context, char **pkg_id);
+
+
+/**
+ * @brief  Gets the application state with the given application context.
+ * @since_tizen 3.0
+ * @param[in]   app_context  The application context
+ * @param[out]  state        The application state of the given application context
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
+ */
+int app_context_get_app_state(app_context_h app_context, app_state_e *state);
+
+
+/**
  * @brief  Checks whether the application with the given application context is terminated.
  * @since_tizen @if MOBILE 2.3 @elseif WEARABLE 2.3.1 @endif
  * @param[in]   app_context  The application context
@@ -139,6 +179,20 @@ int app_context_is_terminated(app_context_h app_context, bool *terminated);
  */
 int app_context_is_equal(app_context_h lhs, app_context_h rhs, bool *equal);
 
+
+/**
+ * @brief  Checks whether sub application of app group.
+ * @since_tizen 3.0
+ * @param[in]   app_context  The application context
+ * @param[out]  equal @c true if sub application of app group, \n
+ *                    otherwise @c false if leader application of app group
+ * @return      @c 0 on success,
+ *              otherwise a negative error value
+ * @retval  #APP_MANAGER_ERROR_NONE               Successful
+ * @retval  #APP_MANAGER_ERROR_INVALID_PARAMETER  Invalid parameter
+ * @retval  #APP_MANAGER_ERROR_IO_ERROR           I/O error
+ */
+int app_context_is_sub_app(app_context_h app_context, bool *is_sub_app);
 
 /**
  * @brief  Clones the application context handle.
