eba620d4f8a81a03fb084349575c19555d6675ea
Junghyun Yeon <jungh.yeon@samsung.com>
2016-03-14 20:52:55 +0900

    add app signal listening functions at app-manager
    Signed-off-by: Junghyun Yeon <jungh.yeon@samsung.com>
    
    Change-Id: I4a86707a94af2ae4fb4c9b9db1161ca8d09f9d69

--- include/app_manager.h
+++ include/app_manager.h
@@ -54,6 +54,64 @@ typedef enum {
 } app_manager_error_e;
 
 /**
+ * @brief  Enumeration for app manager status type.
+ * @since_tizen 3.0
+ */
+typedef enum {
+	APP_MANAGER_EVENT_STATUS_TYPE_ALL = 0x00,		/**< All Status */
+	APP_MANAGER_EVENT_STATUS_TYPE_ENABLE = 0x01,	/**< Application enable status */
+	APP_MANAGER_EVENT_STATUS_TYPE_DISABLE = 0x02,	/**< Application disable status */
+} app_manager_event_status_type_e;
+
+/**
+ * @brief Enumeration for app manager event type.
+ * @since_tizen 3.0
+ */
+typedef enum {
+	APP_MANAGER_EVENT_ENABLE_APP = 0,	/**< Application enable event */
+	APP_MANAGER_EVENT_DISABLE_APP,		/**< Application disable event. */
+} app_manager_event_type_e;
+
+/**
+ * @brief Enumeration for app manager event state.
+ * @since_tizen 3.0
+ */
+typedef enum {
+	APP_MANAGER_EVENT_STATE_STARTED = 0,	/**< Started event state */
+	APP_MANAGER_EVENT_STATE_COMPLETED,		/**< Completed event state*/
+	APP_MANAGER_EVENT_STATE_FAIL,			/**< Failed event state */
+} app_manager_event_state_e;
+
+/**
+ * @brief  App manager event handle.
+ * @since_tizen 3.0
+ */
+typedef struct _app_manager_event_s *app_manager_event_h;
+
+/**
+ * @brief Called when the app is enabled or disabled.
+ * @since_tizen 3.0
+ *
+ * @param[in] type        The type of the app to be enabled or disabled
+ * @param[in] app     The name of the app to be enabled or disabled
+ * @param[in] event_type  The event type of the app
+ * @param[in] event_state The current event state of the app
+ * @param[in] handle       The app manager event handle
+ * @param[in] user_data   The user data passed from app_manager_event_set_cb()
+ * @pre  This function is called when an application being disabled or enabled, after you register this callback using app_manager_event_set_cb().
+ *
+ * @see app_manager_event_set_cb()
+ * @see app_manager_event_unset_cb()
+ */
+typedef void (*app_manager_event_cb) (
+		const char *type,
+		const char *app,
+		app_manager_event_type_e event_type,
+		app_manager_event_state_e event_state,
+		app_manager_event_h handle,
+		void *user_data);
+
+/**
  * @brief  Called when an application is launched or terminated.
  * @since_tizen @if MOBILE 2.4 @elseif WEARABLE 3.0 @endif
  * @param[in]   app_context  The application context of the application launched or terminated
@@ -108,7 +166,7 @@ int app_manager_set_app_context_event_cb(app_manager_app_context_event_cb callba
 /**
  * @brief   Unregisters the callback function.
  * @since_tizen @if MOBILE 2.4 @elseif WEARABLE 3.0 @endif
- * @see app_manager_set_app_event_cb()
+ * @see app_manager_set_event_cb()
  * @see app_manager_app_context_event_cb()
  */
 void app_manager_unset_app_context_event_cb(void);
@@ -347,6 +405,103 @@ int app_manager_get_shared_trusted_path(const char *app_id, char **path);
  * @retval  #APP_MANAGER_ERROR_OUT_OF_MEMORY      Out of memory
  */
 int app_manager_get_external_shared_data_path(const char *app_id, char **path);
+
+/**
+ * @brief Creates a app manager event handle.
+ * @since_tizen 3.0
+ * @remarks You must release @a handle using app_manager_event_destroy().
+ *
+ * @param[out] handle The app manager event handle that is newly created on success
+ *
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ *
+ * @retval #APP_MANAGER_ERROR_NONE              Successful
+ * @retval #APP_MANAGER_ERROR_OUT_OF_MEMORY     Out of memory
+ * @retval #APP_MANAGER_ERROR_PERMISSION_DENIED Permission denied
+ * @see app_manager_event_destroy()
+ */
+int app_manager_event_create(app_manager_event_h *handle);
+
+/**
+ * @brief Sets the event status of the app when the app is enabled or disabled.
+ *        You can combine multiple status using OR operation which you want to listen.
+ * @since_tizen 3.0
+ *
+ * @param [in] handle     The app manager event handle
+ * @param [in] status_type The status of the application
+ *
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ *
+ * @retval #APP_MANAGER_ERROR_NONE              Successful
+ * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #APP_MANAGER_ERROR_REQUEST_FAILED          Internal error
+ *
+ * @see app_manager_event_status_type_e
+ * @see app_manager_event_set_cb()
+ */
+int app_manager_event_set_status(app_manager_event_h handle, int status_type);
+
+/**
+ * @brief Registers a callback function to be invoked when the app is event has occured
+ * @since_tizen 3.0
+ * @param[in] handle    The app manager event handle
+ * @param[in] callback   The callback function to be registered
+ * @param[in] user_data  The user data to be passed to the callback function
+ *
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ *
+ * @retval #APP_MANAGER_ERROR_NONE              Successful
+ * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #APP_MANAGER_ERROR_PERMISSION_DENIED Permission denied
+ * @retval #APP_MANAGER_ERROR_REQUEST_FAILED          Internal error
+ * @post app_manager_event_cb() will be invoked.
+ *
+ * @see app_manager_event_set_status()
+ * @see app_manager_event_cb()
+ * @see app_manager_event_unset_cb()
+ */
+int app_manager_event_set_cb(app_manager_event_h handle,
+		app_manager_event_cb callback,
+		void *user_data);
+
+/**
+ * @brief Unregisters the callback function.
+ * @since_tizen 3.0
+ *
+ * @param[in] handle The app manager event handle
+ *
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ *
+ * @retval #APP_MANAGER_ERROR_NONE              Successful
+ * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #APP_MANAGER_ERROR_PERMISSION_DENIED Permission denied
+ * @retval #APP_MANAGER_ERROR_REQUEST_FAILED          Internal error
+ *
+ * @see app_manager_event_cb()
+ * @see app_manager_event_set_cb()
+ */
+int app_manager_event_unset_cb(app_manager_event_h handle);
+
+/**
+ * @brief Destroys the app manager event handle.
+ * @since_tizen 3.0
+ *
+ * @param[in] handle The app managerevent handle
+ *
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ *
+ * @retval #APP_MANAGER_ERROR_NONE              Successful
+ * @retval #APP_MANAGER_ERROR_INVALID_PARAMETER Invalid parameter
+ *
+ * @see app_manager_event_create()
+ */
+int app_manager_event_destroy(app_manager_event_h handle);
+
 
 /**
  * @}
